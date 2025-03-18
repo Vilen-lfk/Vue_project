ecc843738dcf9b77ca74ef472160b7d063bb4a2e
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-03-18 21:40:46 +0300

    2.3. Три шага подключения бокового меню  (drawer)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <v-app>
+    <v-navigation-drawer app 
+    v-model="drawer"></v-navigation-drawer>
     <v-navigation-drawer app></v-navigation-drawer>
     <v-app-bar app dark color="primary">
+    <v-app-bar-nav-icon @click="drawer = 
+    !drawer"></v-app-bar-nav-icon>
     <v-app-bar-nav-icon></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
