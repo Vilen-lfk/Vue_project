58d1bb2492554be6c28458f2fded83f7d6dbb68b
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-03-18 20:38:41 +0300

    Чистка стартового проекта

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <h1>Очистили стартовый проект</h1>
 </template>
 
+<script>
+  export default {
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
