1fc3e3f42c2ee09fffc84d42958b566c7c67092b
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-03-18 21:09:54 +0300

    2.2. Добавление иконки к кнопке

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
-   <v-app>
-  <v-navigation-drawer app></v-navigation-drawer> 
-  <v-app-bar app dark color="primary">
+  <v-app>
+    <v-navigation-drawer app></v-navigation-drawer>
+    <v-app-bar app dark color="primary">
     <v-app-bar-nav-icon></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text>Link One</v-btn> 
-       <v-btn  text>Link Two</v-btn> 
-       <v-btn  text>Link Three</v-btn>
+      <v-btn>
+        <v-icon start icon="mdi-bike"></v-icon>
+        Link One
+      </v-btn>
+      <v-btn>Link Two</v-btn>
+      <v-btn>Link Three</v-btn>
     </v-toolbar-items>
-  </v-app-bar>
-  <v-main>
-    
-  </v-main>
-  </v-app>
+    </v-app-bar>
+    <v-main>
+    </v-main>
+    </v-app>
 
 </template>
 
