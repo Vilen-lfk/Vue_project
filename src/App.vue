0aed1c42145b8beb14d17df7454a71120d41d2cc
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-03-19 22:32:14 +0300

    2.4. Верстка меню в дровере

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <template>
   <v-app>
-    <v-navigation-drawer app 
-    v-model="drawer"></v-navigation-drawer>
-    <v-navigation-drawer app></v-navigation-drawer>
-    <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon @click="drawer = 
-    !drawer"></v-app-bar-nav-icon>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn>
-        <v-icon start icon="mdi-bike"></v-icon>
-        Link One
-      </v-btn>
-      <v-btn>Link Two</v-btn>
-      <v-btn>Link Three</v-btn>
-    </v-toolbar-items>
-    </v-app-bar>
-    <v-main>
-    </v-main>
-    </v-app>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+          КИПУ
+          </v-list-item-title>
+          <v-list-item-subtitle>
+          Учебный проект
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item>
+          <template v-slot:prepend>
+            <v-icon icon="mdi-cake-variant"></v-icon>
+          </template>
+          <v-list-item-title>Link One</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer> 
+
+    <v-app-bar app dark color="primary">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn>
+          <v-icon start icon="mdi-cake-variant"></v-icon>
+          Link One
+        </v-btn>
+        <v-btn>Link Two</v-btn> 
+        <v-btn>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  </v-app>
 </template>
+
+
 
 <script>
 export default {
@@ -32,6 +50,7 @@ export default {
   }
 }
 </script>
+
 
 
 
