494e9d7c16f328bc96bf5716bc49c0c93bd6304f
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-03-22 15:57:46 +0300

    4.1. Верстка формы

--- src/views/Auth/RegistrationView.vue
+++ src/views/Auth/RegistrationView.vue
@@ -1,15 +1,73 @@
 <template>
-    <v-container>
-      <v-row justify="center">
-        <h1>reg</h1>
-      </v-row>
-    </v-container>
+  <v-container>
+  <v-row justify="center">
+  <v-col cols="12" sm="8" lg="6">
+        <v-card class="elevetion-12" >
+        <v-toolbar dark color="primary" class="pl-4" >
+        Registration
+        </v-toolbar>
+        <v-card-text>
+        <v-form v-model="valid" ref="form" validation>
+        <v-text-field
+              prepend-icon="mdi-account"
+              name="email"
+              label="Email"
+              type="email"
+              v-model="email"
+              :rules="emailRules">
+              
+        </v-text-field>
+        <v-text-field
+              prepend-icon="mdi-lock"
+              name="password"
+              label="Password"
+              type="password"
+              v-model="password"
+              :rules="passwordRules">
+        </v-text-field>
+        </v-form>
+        </v-card-text>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn 
+            color="primary" 
+            @click="onSubmit" 
+            :disabled="!valid">
+            Create Account
+            </v-btn>
+        </v-card-actions>
+        </v-card>
+  </v-col>
+  </v-row>
+  </v-container>
 </template>
 <script>
 export default {
-  data () {
-    return {
+data() {
+  return {
+    email: "",
+    password: "",
+    valid: false,
+    emailRules: [
+      v => !!v || "Введите E-mail",
+      v => /.+@.+\..+/.test(v) || "Проверьте правильность написания E-mail"
+    ],
+    passwordRules: [
+      v => !!v || "Введите пароль",
+      v => (v && v.length >= 6) || "Пароль должен состоять как минимум из 6 символов"
+    ]
+  };
+},
+methods: {
+  onSubmit() {
+    if (this.$refs.form.validate()) {
+      const user = {
+        email: this.email,
+        password: this.password
+      };
+      console.log(user);
     }
   }
 }
+};
 </script>
