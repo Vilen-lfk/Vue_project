fc2899f060dfa5b011c67743014ef8937dd1e01d
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-03-26 18:21:11 +0300

    5.4. Подключение данных к карусели

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,18 @@
   <v-col cols="12" xs="12">
     <v-carousel>
       <v-carousel-item 
-      cover
-      ></v-carousel-item>
+        v-for="ad in ads"
+        :key="ad.id"
+        :src="ad.src"
+        cover
+      >
+        <div class="ad-link">
+        <v-btn class="error" :to="'/ad/' + ad.id">
+          {{ ad.title }}
+        </v-btn>
+        </div>
+          
+      </v-carousel-item>
     </v-carousel>
   </v-col>
   </v-row>
@@ -35,7 +45,19 @@
   </v-row>
   </v-container>
 </template>
-  
+<style scoped>
+	.ad-link {
+		position:absolute;
+		bottom: 50px;
+		left:50%;
+		background: rgb(0,0,0,0.5);
+		transform: translate(-50%,0);
+		padding: 5px 10px;
+		border-top-right-radius: 5px;
+		border-top-left-radius: 5px;
+	}
+</style>
+
 
 <script>
 export default {
@@ -46,28 +68,28 @@ export default {
 				title:"First",
 				desc:"First Desc",
 				promo: true,
-				src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+				src: "https://i.pinimg.com/736x/83/7d/3e/837d3e2e09c9bfdcecfe9b3d5a14ec7c.jpg",
 				id:"1"
 			},
 			{
 				title:"Second",
 				desc:"Second Desc",
 				promo: true,
-				src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+				src: "https://cdn-0.totalmotorcycle.com/wp-content/uploads/2023/07/2024-Ducati-Panigale-V2a-1200x800.jpg",
 				id:"2"
 			},
 			{
 				title:"Third",
 				desc:"Thitd Desc",
 				promo: true,
-				src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+				src: "https://i.pinimg.com/originals/c4/b9/46/c4b946640b0ad8ac134291cf748970f4.jpg",
 				id:"3"
 			},
 			{
 				title:"Fouth",
 				desc:"Fouth Desc",
 				promo: true,
-				src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+				src: "https://i.pinimg.com/736x/52/0f/31/520f31c039b768b839b2a72cd8d33160.jpg",
 				id:"4"
 			}
 		]
