2d2e65da30d752b0da03fc606fd5224c3ab69879
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-03-26 18:59:48 +0300

    5.5. Подключение данных к grid

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-container>
+  <v-container grid-list-lg>
+
   <v-row justify="center">
   <v-col cols="12" xs="12">
     <v-carousel>
@@ -21,43 +22,40 @@
   </v-row>
   <v-row justify="center">
   <v-col cols="12" xs="12">
+    <v-layout row wrap> 
+		<v-flex xs12 sm6 md4
+			v-for="ad in ads"
+			:key="ad.id"
+		>  
   <v-card>
         <v-img
+        :src="ad.src"
         height="200px"
         ></v-img>
         <v-card-title primary-title>
         <div>
         <h3
         class="headline mb0"
-        >Здесь будет заголовок</h3>
-        <div>Здесь будет описание</div>
+        >{{ad.title}}</h3>
+        <div>{{ad.desc}}</div>
         </div>
         </v-card-title>
         <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn text>Open</v-btn>
+        <v-btn text :to="'/ad/' + ad.id">Open</v-btn>
         <v-btn raised color="primary">
         Buy
         </v-btn>
         </v-card-actions>
         </v-card>
+      </v-flex>
+</v-layout> 
+
   </v-col>
   </v-row>
   </v-container>
+  
 </template>
-<style scoped>
-	.ad-link {
-		position:absolute;
-		bottom: 50px;
-		left:50%;
-		background: rgb(0,0,0,0.5);
-		transform: translate(-50%,0);
-		padding: 5px 10px;
-		border-top-right-radius: 5px;
-		border-top-left-radius: 5px;
-	}
-</style>
-
 
 <script>
 export default {
@@ -86,7 +84,7 @@ export default {
 				id:"3"
 			},
 			{
-				title:"Fouth",
+				title:"Fourth",
 				desc:"Fouth Desc",
 				promo: true,
 				src: "https://i.pinimg.com/736x/52/0f/31/520f31c039b768b839b2a72cd8d33160.jpg",
@@ -98,3 +96,15 @@ export default {
 
 }
 </script>
+<style scoped>
+	.ad-link {
+		position:absolute;
+		bottom: 50px;
+		left:50%;
+		background: rgb(0,0,0,0.5);
+		transform: translate(-50%,0);
+		padding: 5px 10px;
+		border-top-right-radius: 5px;
+		border-top-left-radius: 5px;
+	}
+</style>
