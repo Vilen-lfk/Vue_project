783820b888b16480806bbea25070f7ee06f7afab
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-04-04 12:51:56 +0300

    7.2. Объявление переменных и метода

--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -71,17 +71,28 @@
 
 
 export default {
-  data () {
+  data () { 
     return {
-      
-        title: "...",
-        desc:"...",
-        promo: true,
-        src: "...",
-        id:""
-        }
+          valid: false,
+          title: "",
+          description: "",
+          promo: true
+                } 	
+      },
+      methods: {
+        createAd(){
+          if (this.$refs.form.validate()){
+            const ad = {
+              title: this.title,
+              desc: this.description,
+              promo: this.promo
+            }
+            console.log(ad)
+            }
+        }     
 
-    }
+  }
+
   }
 
 </script>
