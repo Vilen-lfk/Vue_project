ed90e81d577e03969b6f0ba7e02ace8f6735ed1b
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-04-10 09:01:32 +0300

    10.1.  Установка и подключение Vuex

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import router from './router'
+import store from './store'  // Импортируем уже существующий store
+
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(store)  // Используем уже импортированный store
+  .use(vuetify)
+  .mount('#app')
+
+const store = createStore({
+modules:{
+}
+})
+
+export default store
+
