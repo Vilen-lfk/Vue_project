f5cbbb5f82e30f38c17a93040081e7f284e033d7
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-04-15 09:34:01 +0300

    10.5.  Просмотр деталей объявления

--- src/views/Ads/AdView.vue
+++ src/views/Ads/AdView.vue
@@ -6,12 +6,12 @@
               <v-card class="mt-5">
                 <v-img
                 height="400px"
-                src="https://i.pinimg.com/736x/83/7d/3e/837d3e2e09c9bfdcecfe9b3d5a14ec7c.jpg"
+                :src="ad.src"
                 ></v-img>
                 <v-card-text>
-                <h1 class="text--primary mb-5">Suzuki GSX-R1000</h1>
+                <h1 class="text--primary mb-5">{{ ad.title }}</h1>
                 <p>
-                  Suzuki GSX-R1000 (часто называемый Gixxer) — это спортивный мотоцикл производства Suzuki.[1] Он был представлен в 2001 году для замены GSX-R1100 и оснащен жидкостным охлаждением 999 куб.см (61,0 кубических дюйма) поперечным четырехцилиндровым, четырехтактным двигателем, хотя первоначально с 2001 по 2004 год он составлял 988 куб. см (60,3 кубических дюйма).
+                  {{ ad.desc }}
                 </p>
                 </v-card-text>
                 <v-card-actions>
@@ -27,8 +27,16 @@
 </template>
 <script>
 export default {
+  props: ['id'],
   data () {
     return {
+    computed: {
+		ad() {
+		const id = this.id
+		return this.$store.getters.adById(id)
+		}
+	}
+
     }
   }
 }
