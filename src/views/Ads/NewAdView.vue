cedc8137a06c3bd0f9d4aa01995f2a42c9c87fe5
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-05-27 10:46:14 +0300

    mt_11_1

--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -85,7 +85,8 @@ export default {
             const ad = {
               title: this.title,
               desc: this.description,
-              promo: this.promo
+              promo: this.promo,
+              src: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
             }
             console.log(ad)
             }
