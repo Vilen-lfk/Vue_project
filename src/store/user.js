feb3dad84576a36b1719e8a22a9f252183c427ac
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-05-27 13:55:09 +0300

    mt_13_4

--- src/store/user.js
+++ src/store/user.js
@@ -1,44 +1,43 @@
 class User {
-    constructor(id, email, password) {
-        this.id = id
-        this.email = email
-        this.password = password
-    }
+  constructor(id, email, password) {
+    this.id = id
+    this.email = email
+    this.password = password
+  }
 }
 
 export default {
-	state: {
-     user: null
-},
-	mutations: {
-        setUser(state,payload) {
-        console.log(payload)
-        state.user = payload
-        }
-},
-	actions: {
-        registerUser({commit},{email, password}){
-        //Здесь запрос на сервер для регистрации
-        commit('setUser', new User(1, email, 	password));
-        commit('clearError')
-	    commit('setLoading', true)
-	    fb.auth()
-          .createUserWithEmailAndPassword(email,password)
-          .then(response => {
-            commit('setUser', new User(response.user.uid))
-            commit('setLoading', false)
-	    })
-        .catch( error => {
-		    commit('setLoading', false)
-		    commit('setError', error.message)
-            throw error
-	    });
-
+  state: {
+    user: null
+  },
+  mutations: {
+    setUser(state, payload) {
+      console.log(payload)
+      state.user = payload
     },
-},
-    getters: {
-     user(state) {
-return state.user
-	}
-}
+  },
+  actions: {
+    async registerUser({ commit }, { email, password }) {
+      try {
+        commit('clearError')
+        commit('setLoading', true)
+
+        // Заменить на свой HTTP-запрос к бэкенду
+        const fakeId = Date.now()
+        const user = new User(fakeId, email, password)
+        commit('setUser', user)
+
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setLoading', false)
+        commit('setError', error.message)
+        throw error
+      }
+    }
+  },
+  getters: {
+    user(state) {
+      return state.user
+    }
+  }
 }
