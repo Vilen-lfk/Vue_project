c3eb8317c7df83fef41d4f189ebd9f64786bab56
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-05-29 18:50:07 +0300

    mt_14_4

--- src/App.vue
+++ src/App.vue
@@ -54,6 +54,22 @@
     <v-main>
     <router-view></router-view>
     </v-main>
+    <v-snackbar
+v-model="error"
+multi-line
+:timeout="2000"
+color="primary"
+>
+{{ error }}
+<template v-slot:actions>
+<v-btn
+variant="text"
+@click="closeError"
+>
+Close
+</v-btn>
+</template>
+</v-snackbar>
   </v-app>
 
   
@@ -74,7 +90,19 @@ data() {
       {title:"My ads", icon:"mdi-view-list-outline", url:"/list"}
       ]
       }
-      }
+      },
+      computed: {
+    error () {
+      return this.$store.getters.error
+   }
+},
+methods: {
+    closeError () {
+      this.$store.dispatch('clearError')
+    }
+ }
+
+
 }
 </script>
 
