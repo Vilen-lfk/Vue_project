b56a353119db695731b568f91989e290b8abb3aa
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-06-19 22:12:28 +0300

    mt_18_4

--- src/views/Shared/BuyAdModal.vue
+++ src/views/Shared/BuyAdModal.vue
@@ -1,86 +1,108 @@
 <template>
-<v-dialog
-v-model="modal"
-width="400px">
-<template v-slot:activator="{ props }">
-<v-btn
-v-bind="props"
-color="warning">
-Buy</v-btn></template>
-<v-card class="pa-3">
-<v-row justify="center">
-<v-col cols="12">
-<v-card-title>
-<h1 class="text--primary">Do you wan't to buy it?</h1>
-</v-card-title>
-</v-col>
-</v-row>
+  <v-dialog v-model="modal" width="400px">
+    <template v-slot:activator="{ props }">
+      <v-btn v-bind="props" color="warning">Buy</v-btn>
+    </template>
 
-<v-row justify="center">
-<v-col cols="12">
-<v-card-text>
-<v-text-field
-name="name"
-label="Your name"
-type="text"
-v-model="name"
->
-</v-text-field>
-<v-text-field
-name="phone"
-label="Your phone"
-type="text"
-v-model="phone"
->
-</v-text-field>
-</v-card-text>
-</v-col>
-</v-row>
+    <v-card class="pa-3">
+      <v-row justify="center">
+        <v-col cols="12">
+          <v-card-title>
+            <h1 class="text--primary">Do you want to buy it?</h1>
+          </v-card-title>
+        </v-col>
+      </v-row>
 
-<v-row justify="center">
-<v-col cols="12">
-<v-card-actions>
-<v-spacer></v-spacer>
-<v-btn @click="onClose">Close</v-btn>
-<v-btn @click="onSave" color="success">Buy It!</v-btn>
-</v-card-actions>
-</v-col>
-</v-row>
-</v-card>
-</v-dialog>
+      <v-row justify="center">
+        <v-col cols="12">
+          <v-card-text>
+            <v-text-field
+              name="name"
+              label="Your name"
+              type="text"
+              v-model="name"
+            />
+            <v-text-field
+              name="phone"
+              label="Your phone"
+              type="text"
+              v-model="phone"
+            />
+          </v-card-text>
+        </v-col>
+      </v-row>
+
+      <v-row justify="center">
+        <v-col cols="12">
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="onClose" :disabled="localLoading">Close</v-btn>
+            <v-btn
+              @click="onSave"
+              :disabled="localLoading"
+              :loading="localLoading"
+              color="success"
+            >
+              Buy It!
+            </v-btn>
+          </v-card-actions>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 export default {
-props: ['ad'],
-data() {
-return {
-modal: false,
-name:'',
-phone:''
-}
-},
-methods:{
-    onClose(){
-        this.name=""
-        this.phone =""
-        this.modal = false
-    },
-    onSave(){
-        if(this.name!== "&& this.phone !==")
-        this.$store.dispatch('createOrder',{
-    name:this.name,
-    phone:this.phone,
-    adid:this.id,
-    userid:this.userid,
-    })
-    .finally(() =>{
-        this.name = ""
-        this.phone = ""
-        this.modal = false
-    })
-    this.modal = false
+  props: ['ad'],
+  data() {
+    return {
+      modal: false,
+      name: '',
+      phone: '',
+      localLoading: false
     }
+  },
+  methods: {
+    onClose() {
+      this.name = ''
+      this.phone = ''
+      this.modal = false
+    },
+    onSave() {
+  if (this.name.trim() !== '' && this.phone.trim() !== '') {
+    this.localLoading = true
+
+    const result = this.$store.dispatch('createOrder', {
+      name: this.name,
+      phone: this.phone,
+      adid: this.ad.id,
+      userid: this.ad.userId,
+    })
+
+    if (result && typeof result.then === 'function') {
+      result
+        .then(() => {
+          this.name = ''
+          this.phone = ''
+          this.modal = false
+        })
+        .catch(err => {
+          console.error('Order creation failed:', err)
+        })
+        .finally(() => {
+          this.localLoading = false
+        })
+    } else {
+      console.warn('createOrder did not return a Promise')
+      this.localLoading = false
+      this.modal = false
+    }
+  } else {
+    alert("Please fill in both name and phone fields.")
+  }
 }
+
+  }
 }
 </script>
