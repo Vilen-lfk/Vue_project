530432cff5e1718a817711f80dbda7c887740345
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-06-19 21:46:20 +0300

    mt_18_1

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
 import store from './store'  // Импорт Vuex store
+import BuyAdModal from './views/Shared/BuyAdModal.vue'
 
 loadFonts()
 
@@ -11,4 +12,5 @@ createApp(App)
   .use(router)
   .use(store)      // <-- подключение хранилища
   .use(vuetify)
+  .component("buy-ad-modal", BuyAdModal)
   .mount('#app')
--- src/store/ads.js
+++ src/store/ads.js
@@ -29,74 +29,66 @@ export default {
         title: "Fouth",
         desc: "Fouth Desc",
         promo: true,
-        src:  "https://i.pinimg.com/736x/52/0f/31/520f31c039b768b839b2a72cd8d33160.jpg",
+        src: "https://i.pinimg.com/736x/52/0f/31/520f31c039b768b839b2a72cd8d33160.jpg",
         id: "4",
         userId: "4"
-      },
-    ],
+      }
+    ]
   },
-mutations: {
-		createAd(state, payload){
-			state.ads.push(payload)
-		},
-	    loadAds (state, payload) {
-	      state.ads = payload
-	    },
-	    updateAd (state, {title, desc, id}) {
-	    	const ad = state.ads.find(a => {
-	    		return a.id === id
-	    	})
-	    	ad.title = title
-	    	ad.desc = desc
-	}
-},
-	actions: {
-		createAd({commit},payload){
-			payload.id = Math.random()
-			commit('createAd', payload)
-		},
-    async updateAd ({commit},{title,desc,id}) {
+  mutations: {
+    createAd(state, payload) {
+      state.ads.push(payload)
+    },
+    loadAds(state, payload) {
+      state.ads = payload
+    },
+    updateAd(state, { title, desc, id }) {
+      const ad = state.ads.find(a => a.id === id)
+      if (ad) {
+        ad.title = title
+        ad.desc = desc
+      }
+    }
+  },
+  actions: {
+    createAd({ commit }, payload) {
+      payload.id = Math.random()
+      commit('createAd', payload)
+    },
+    async updateAd({ commit }, { title, desc, id }) {
       commit('clearError')
       commit('setLoading', true)
-      //Заглушка запроса
-    let isRequestOk = true
-      let promise = new Promise(function(resolve) {
-      resolve('Done')
-      });
-    if (isRequestOk) {
-      await promise.then(()=> {
-      commit('updateAd',{ title, desc, id})
-      commit('setLoading', false)
-      })
-    } else {
-      await promise.then(()=> {
-      commit('setLoading', false)
-      commit('setError', 'Ошибка редактирования объявления')
-      throw 'Упс... Ошибка редактирования объявления'
-    })
-    }
-    }
-	},
-	getters: {
-		ads(state) {
-			return state.ads
-		},
-		promoAds(state) {
-			return state.ads.filter(ad => {
-				return ad.promo
-			})
-		},
-		myAds(state, getters) {
-			return state.ads.filter(ad  => {
-        return ad.userId == getters.user.id
-      })
-		},
-		adById(state) {
-			return id => {
-				return state.ads.find(ad => ad.id == id)
-			}
-		}
 
-	}
+      // Заглушка запроса
+      const isRequestOk = true
+      const promise = new Promise(resolve => resolve('Done'))
+
+      if (isRequestOk) {
+        await promise.then(() => {
+          commit('updateAd', { title, desc, id })
+          commit('setLoading', false)
+        })
+      } else {
+        await promise.then(() => {
+          commit('setLoading', false)
+          commit('setError', 'Ошибка редактирования объявления')
+          throw 'Упс... Ошибка редактирования объявления'
+        })
+      }
+    }
+  },
+  getters: {
+    ads(state) {
+      return state.ads
+    },
+    promoAds(state) {
+      return state.ads.filter(ad => ad.promo)
+    },
+    myAds(state, getters) {
+      return state.ads.filter(ad => ad.userId == getters.user.id)
+    },
+    adById(state) {
+      return id => state.ads.find(ad => ad.id == id)
+    }
+  }
 }
-
